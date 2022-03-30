1936fb4c833d5f1112d9747d0f549e53ae6139a6
0xsaxit <bcsaxit@gmail.com>
2022-03-30 16:43:01 +0200

    Remove unused IPFS code left over from previous integration

--- platform/scripts/start.sh
+++ platform/scripts/start.sh
@@ -19,7 +19,3 @@ done
 npm run contracts:deploy:local
 
 npm run graph:all:local
-
-docker exec platform_ipfs_1 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin  '["http://localhost:8080"]'
-docker exec platform_ipfs_1 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST", "GET"]'
-docker restart platform_ipfs_1

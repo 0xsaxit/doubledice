f50e103b9b29cefc39ad15faaa8548ba80fa6126
0xsaxit <bcsaxit@gmail.com>
2022-04-28 19:22:06 +0200

    Revert "verify: Delete some dev comments prior to verification"
    This reverts commit c32ec6f1dbe41f3828ed697f96dd8ab8dcca180e.

--- platform/contracts/VirtualFloorMetadataValidator.sol
+++ platform/contracts/VirtualFloorMetadataValidator.sol
@@ -7,6 +7,10 @@ import "./library/Utils.sol";
 
 /**
  * @notice In v1 the metadata is a direct ABI-encoding of this structure.
+ * @dev Potential alternative encodings:
+ * - Direct JSON-encoding
+ * - SHA-256 of JSON-encoding
+ * - SHA-256 of JSON-encoding + validation signature
  */
 struct VirtualFloorMetadataV1 {
     string category;

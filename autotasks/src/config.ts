57a562116e6431617610d449cac531c2e2f15f9d
0xsaxit <bcsaxit@gmail.com>
2022-05-14 15:54:42 +0200

    autotask: :sparkles: New env "dev"

--- autotasks/src/config.ts
+++ autotasks/src/config.ts
@@ -1,10 +1,21 @@
 export const DD_ENV = process.env.DD_ENV;
 
-if (!(DD_ENV === 'beta' || DD_ENV === 'live')) {
-  throw new Error(`Unexpected DD_ENV "${DD_ENV}"; must be "beta" or "live"`);
+if (!(DD_ENV === 'dev' || DD_ENV === 'beta' || DD_ENV === 'live')) {
+  throw new Error(`Unexpected DD_ENV "${DD_ENV}"; must be "dev" or "beta" or "live"`);
 }
 
 const configs = {
+  dev: {
+    GRAPHQL_ENDPOINT: 'https://api.thegraph.com/subgraphs/name/doubledicedev/doubledice-mumbai',
+    APP_ORIGIN: 'https://dd-mumbai-dev.netlify.app',
+    // See https://doubledice.slack.com/services/B03AUCBPLJU
+    SLACK_WEBHOOK_ENDPOINT: 'https://hooks.slack.com/services/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',
+    BLOCK_EXPLORER_HOST: 'https://mumbai.polygonscan.com',
+    DOUBLEDICE_CONTRACT_ADDRESS: '0xCE3CD58c9600B942a66B04e55CC81e27395b4555',
+    LOG_NO_ACTION: false,
+    // Generated once via hexlify(randomBytes(32)).slice(2)
+    DISCORD_MAINTAINER_API_KEY: '41b6fd05d9c050c0695d635e6ed9f47c023be9a8354107f3c0e38c466a25ea40',
+  },
   beta: {
     GRAPHQL_ENDPOINT: 'https://api.thegraph.com/subgraphs/name/doubledicedev/doubledice-mumbai2',
     APP_ORIGIN: 'https://beta.doubledice.com',
